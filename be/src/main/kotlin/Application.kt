6ba43ac37791e47b52b6b45d0249fc7a7b58c2ef
Marco <fracassi.marco@gmail.com>
2021-06-25 18:03:22 +0200

    Sync multiple sockets connections

--- be/src/main/kotlin/Application.kt
+++ be/src/main/kotlin/Application.kt
@@ -3,19 +3,41 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.server.netty.*
 import io.ktor.websocket.*
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.LinkedHashSet
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     install(WebSockets)
+    Repository()
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
+            val thisConnection = Connection(this)
+            connections += thisConnection
             for(frame in incoming) {
                 frame as? Frame.Text ?: continue
                 val receivedText = frame.readText()
                 println(receivedText)
-                send(receivedText)
+                connections.forEach {
+                    it.session.send(receivedText)
+                }
             }
         }
     }
+}
+
+class Repository {
+    companion object {
+        val messages = mutableListOf<String>()
+    }
+}
+
+class Connection(val session: DefaultWebSocketSession) {
+    companion object {
+        var lastId = AtomicInteger(0)
+    }
+    val name = "user${lastId.getAndIncrement()}"
 }
--- be/src/test/kotlin/IntegrationTest.kt
+++ be/src/test/kotlin/IntegrationTest.kt
@@ -3,33 +3,48 @@ import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
 import io.ktor.server.netty.*
-import io.ktor.util.*
 import junit.framework.Assert.assertEquals
-import kotlinx.coroutines.*
-
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 
 class IntegrationTest {
     @Test
     fun `client server conversation`() {
-        GlobalScope.async {EngineMain.main(emptyArray())}
+        GlobalScope.launch { EngineMain.main(emptyArray()) }
 
         Thread.sleep(10000)
-        val client = HttpClient {
+        val client1 = HttpClient {
+            install(WebSockets)
+        }
+        val client2 = HttpClient {
             install(WebSockets)
         }
         runBlocking {
-            client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-                val received = arrayListOf<String>()
-                val textMessages = listOf("HELLO", "WORLD")
-                for (msg in textMessages) {
-                    outgoing.send(Frame.Text(msg))
-                    received.add((incoming.receive() as Frame.Text).readText())
+            launch {
+                client1.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+                    val received = arrayListOf<String>()
+                    val textMessages = listOf("HELLO", "WORLD")
+                    for (msg in textMessages) {
+                        outgoing.send(Frame.Text(msg))
+                        received.add((incoming.receive() as Frame.Text).readText())
+                    }
+                    assertEquals(listOf("HELLO", "WORLD"), received)
                 }
-                assertEquals(listOf("HELLO", "WORLD"), received)
+            }
+            launch {
+                client2.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+                    val received = arrayListOf<String>()
+                    while (received != listOf("HELLO", "WORLD")) {
+                        received.add((incoming.receive() as Frame.Text).readText())
+                    }
+                }
             }
         }
-        client.close()
+        client1.close()
+        client2.close()
     }
 }
